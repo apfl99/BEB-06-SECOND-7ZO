f7fe432a7fa8d783e3dae064f228ebd7f28f9b6f
apfl99 <rmrehd604@gmail.com>
2022-11-02 10:36:12 +0900

    import contract

---
+++ contract/truffle-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "5777"
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.8.10",
+      settings: {
+        optimizer: {
+          enabled: true, // Default: false
+          runs: 200      // Default: 200
+        },
+      }
+    }
+  }
+};
